c41431f08c39ef229e097b65e0ebb1644ea4ce15
Taehun Kim <taehoon1018@postech.ac.kr>
2022-11-08 20:03:26 +0900

    delete unused files / readme update / backboen map location

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Lane Segmentation
 
-![Teaser](./Teaser.gif)
+This project is built based on [InSPyReNet](https://github.com/plemeri/InSPyReNet)
+
+![Teaser](./figures/Teaser.gif)
 
 ## 1. Create environment
   + Create conda environment with following command `conda create -y -n lane python=3.8`
@@ -8,8 +10,14 @@
   + Install requirements with following command `pip install -r requirements.txt`
 
 ## 2. Preparation
+  ### Pre-trained Checkpoint
+  + Download ImageNet pre-trained checkpoint for backbone network from [Link](https://postechackr-my.sharepoint.com/:u:/g/personal/taehoon1018_postech_ac_kr/EdnCVk9__w1Gh5npELiIWSIBO9DpZhHoiSLZUfGtUkwn3g?e=9zLWtn&download=1)
   + Download checkpoint from [Link](https://postechackr-my.sharepoint.com/:u:/g/personal/taehoon1018_postech_ac_kr/EaKumdLe9iBHv1OWkjisoZ4B9ppCSvs0yZ6pxllgnGorfQ?e=9dp81y&download=1)
   + Move file as follows `./snapshots/HighwayLane/latest.pth`. Create folder if needed.
+
+  ### Dataset
+  + [Train](https://postechackr-my.sharepoint.com/:u:/g/personal/taehoon1018_postech_ac_kr/EfUnpxrl8jRMklEcHmp1cTcBHlQhZhSl7soRNbG0jjLb8w?e=Tj5hbe&download=1)
+  + [Test](https://postechackr-my.sharepoint.com/:u:/g/personal/taehoon1018_postech_ac_kr/ERVEPxwzk2ZElqM7-n05COoBjcztlOnqar1bNd19tlA3Qg?e=wm6Tzh&download=1)
 
 ## 3. Inference
   + Prepare your image folder
--- run/Train.py
+++ run/Train.py
@@ -19,24 +19,11 @@ from utils.dataloader import *
 from lib.optim import *
 from lib import *
 
-def _args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--config', type=str,
-                        default='configs/InSPyReNet_SwinB.yaml')
-    parser.add_argument('--local_rank', type=int, default=-1)
-    parser.add_argument('--verbose', action='store_true', default=False)
-    parser.add_argument('--debug', action='store_true',   default=False)
-    return parser.parse_args()
-
-
 def train(opt, args):
-    device_ids = os.environ["CUDA_VISIBLE_DEVICES"].split(',')
-    device_num = len(device_ids)
-
     train_dataset = eval(opt.Train.Dataset.type)(
         root=opt.Train.Dataset.root, transform_list=opt.Train.Dataset.transform_list)
 
-    if device_num > 1:
+    if args.device_num > 1:
         torch.cuda.set_device(args.local_rank)
         dist.init_process_group(backend='nccl')
         train_sampler = torch.utils.data.distributed.DistributedSampler(
@@ -55,7 +42,7 @@ def train(opt, args):
     model = eval(opt.Model.name)(channels=opt.Model.channels,
                                  pretrained=opt.Model.pretrained)
 
-    if device_num > 1:
+    if args.device_num > 1:
         model = nn.SyncBatchNorm.convert_sync_batchnorm(model)
         model = model.cuda()
         model = nn.parallel.DistributedDataParallel(model, device_ids=[
@@ -101,7 +88,7 @@ def train(opt, args):
         if args.local_rank <= 0 and args.verbose is True:
             step_iter = tqdm.tqdm(enumerate(train_loader, start=1), desc='Iter', total=len(
                 train_loader), position=1, leave=False, bar_format='{desc:<5.5}{percentage:3.0f}%|{bar:40}{r_bar}')
-            if device_num > 1:
+            if args.device_num > 1:
                 train_sampler.set_epoch(epoch)
         else:
             step_iter = enumerate(train_loader, start=1)
@@ -132,7 +119,7 @@ def train(opt, args):
             os.makedirs(os.path.join(
                 opt.Train.Checkpoint.checkpoint_dir, 'debug'), exist_ok=True)
             if epoch % opt.Train.Checkpoint.checkpoint_epoch == 0:
-                torch.save(model.module.state_dict() if device_num > 1 else model.state_dict(
+                torch.save(model.module.state_dict() if args.device_num > 1 else model.state_dict(
                 ), os.path.join(opt.Train.Checkpoint.checkpoint_dir, 'latest.pth'))
 
             if args.debug is True:
@@ -141,11 +128,11 @@ def train(opt, args):
                     opt.Train.Checkpoint.checkpoint_dir, 'debug', str(epoch) + '.png'), debout)
 
     if args.local_rank <= 0:
-        torch.save(model.module.state_dict() if device_num > 1 else model.state_dict(
+        torch.save(model.module.state_dict() if args.device_num > 1 else model.state_dict(
         ), os.path.join(opt.Train.Checkpoint.checkpoint_dir, 'latest.pth'))
 
 
 if __name__ == '__main__':
-    args = _args()
+    args = parse_args()
     opt = load_config(args.config)
     train(opt, args)
